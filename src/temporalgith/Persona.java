296736749be630fb2982fef453d0fbfe7f17bedc
WINDOWS10PRO <WINDOWS10PRO@DESKTOP-1B2228K>
2022-03-27 14:59:25 -0500

    Version 02 de proyecto

---
+++ src/temporalgith/Persona.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package temporalgith;
+
+/**
+ *
+ * @author WINDOWS10PRO
+ */
+public class Persona {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "El nombre es: "+this.nombre;
+    }
+    
+}
